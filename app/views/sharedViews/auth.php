a0be14db2254a2f489b26ac178f3170542f69937
Mehdi Mamouni <km_mamouni@esi.dz>
2024-12-15 20:37:43 +0100

    here we go again

--- app/views/sharedViews/auth.php
+++ app/views/sharedViews/auth.php
@@ -8,34 +8,33 @@ class Auth
 
         <!DOCTYPE html>
         <html lang="en">
+
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>ElMountada</title>
-
+         <script src="https://cdn.tailwindcss.com"></script>
             <link href="public/dist/styles.css" rel="stylesheet">
         </head>
 
         <body>
 
-            <div class="flex h-screen  bg-no-repeat bg-cover justify-center xl:bg-white">
+            <div class="flex h-screen  justify-center xl:bg-bg">
 
 
-            <div class=" hidden x:block xl:flex xl:items-center xl:justify-center xl:p-[25px] xl:w-[52%] "
-            >
-                        <img src="public/assets/charity2.jpg" alt="Login Image"
-                            class="rounded-[25px] h-full w-auto"/>  
-              </div>
-
+                <div class=" hidden xl:block  xl:relative  xl:p-[25px] xl:w-[52%] ">
+                <div class="absolute inset-0 m-[25px] rounded-[25px] bg-principale/60 bg-opacity-50 z-10"> </div>  
+                    <img src="public/assets/charity2.jpg" alt="Login Image" class="rounded-[25px] h-full w-auto" />
+                </div>
+                
                 <div
-                    class="w-full xl:w-[48%] xl:m-0 m-8 flex flex-col justify-center bg-white px-[25px] sm:px-[50px] md:px-[107px] gap-[60px] xl:overflow-hidden xl:h-full z-20"
-                    >
+                    class="w-full xl:w-[48%] xl:m-0 m-8 flex flex-col justify-center bg-bg px-[25px] sm:px-[50px] md:px-[107px] gap-[60px] xl:overflow-hidden xl:h-full z-20">
 
-                    <div class="flex flex-col items-start justify-start px-[30px] ">
-                        <h1 class="font-poppins font-semibold mb-4 text-[24px] text-black">
+                    <div class="flex flex-col items-center justify-center px-[30px] ">
+                        <h1 class="font-poppins font-semibold mb-4 text-[24px] text-text">
                             Bienvenue! <span class="wave-emoji">👋</span>
                         </h1>
-                        <p class="font-openSans font-regular text-[16px]  text-black text-opacity-80 text-start">
+                        <p class="font-openSans font-regular text-[16px]  text-text text-opacity-80 text-center">
                             Connectez-vous à votre compte en remplissant le formulaire de
                             connexion avec vos informations personnelles.
                         </p>
@@ -46,7 +45,7 @@ class Auth
                             <div>
                                 <label>
                                     <input
-                                        class="sm:p-[20px] p-[15px] w-full rounded-[15px] text-[16px] font-openSans font-regular border border-BorderWithoutAction focus-within:border-PrimaryBlack focus:outline-none"
+                                        class="sm:p-[20px] bg-transparent p-[15px] w-full rounded-[10px] text-[16px] font-openSans font-regular border border-primary/30 focus-within:border-primary focus:outline-none"
                                         type="email" placeholder="Adresse email"
                                         pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" />
                                 </label>
@@ -54,22 +53,24 @@ class Auth
 
                             <div>
                                 <div
-                                    class="flex sm:p-[20px] p-[15px] w-full text-[16px]  rounded-[15px] font-openSans font-regular border border-BorderWithoutAction focus-within:border-PrimaryBlack justify-between">
-                                    <input class="bg-transparent border-transparent" id="password" name="password" minLength={8}
+                                    class="flex sm:p-[20px] p-[15px] w-full text-[16px] rounded-[10px] font-openSans font-regular border border-primary/30 focus-within:border-primary justify-between">
+                                    <input class="bg-transparent border-transparent "
+                                    type="password"
+                                    id="password" name="password" minLength={8}
                                         maxLength={20} placeholder="Mot de passe" />
                                 </div>
                             </div>
-                            
-                            <div> 
-                            <button type="submit"
-                                class="w-full hover:bg-PrimaryBlack/80 bg-PrimaryBlack p-[12px] sm:p-[16px] text-white rounded-[30px] font-poppins text-[20px] font-medium">
-                                Se connecter
-                            </button>
+
+                            <div>
+                                <button type="submit"
+                                    class="w-full hover:bg-primary/80 bg-primary p-[12px] sm:p-[16px] text-white rounded-[20px] font-poppins text-[20px] font-medium">
+                                    Se connecter
+                                </button>
                             </div>
                         </div>
                     </form>
                     <p class=" flex justify-center items-center font-openSans text-center text-[14px] text-PrimaryBlack/80">
-                   ElMountada © 2024. tous droits réservés.
+                        ElMountada © 2024. tous droits réservés.
                     </p>
                 </div>
 
@@ -77,10 +78,11 @@ class Auth
 
 
         </body>
+     
 
         </html>
         <?php
     }
-    
+
 }
 ?>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,21 +8,23 @@ module.exports = {
     "./dist/**/*.html",
   ],
   theme: {
-    extend: {},
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
+    extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+      },
+      colors: {
+        text: '#264653',
+          bg2: '#E9C46A',
+          primary: '#E76F51',
+          bg: '#F4F1DE',
+          principale: '#2A2A2A',
+      },
     },
-    colors: {
-      PrimaryBlack: '#1D1C1C',
-      BorderWithoutAction: '#E0E1E2',
-      green: '#0F5012',
-      white: '#FEFEFE',
-      delete: '#FF3A3A',
-    },
+    
   },
   plugins: [],
 }
